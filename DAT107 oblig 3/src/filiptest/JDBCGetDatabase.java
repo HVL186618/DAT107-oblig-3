573f75f16e204b0afa1a26630b62a687719af6a6
HVL186618 <186618@stud.hvl.no>
2025-03-23 14:30:33 +0100

    Updated files

---
+++ DAT107 oblig 3/src/filiptest/JDBCGetDatabase.java
@@ -0,0 +1,33 @@
+package filiptest;
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCGetDatabase {
+	static final String JDBC_Driver = "org.postgresql.Driver";
+	static final String Database = "186618";
+	static final String Brukernavn = "186618";
+	static final String Passord = "test";
+	
+	static final String Tjener_og_port = "ider-database.westeurope.cloudapp.azure.com";
+	static final String DB_URL = "jdbc:postgresql://" + Tjener_og_port + "/" + Database;
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		// TODO Auto-generated method stub
+		String SQL = ""; //Fyll inn.
+		
+		Connection conn = null;
+		Statement stmnt = null;
+		Class.forName(JDBC_Driver);
+	 	try {
+			DriverManager.getConnection(DB_URL, Brukernavn, Passord);
+			System.out.println(DriverManager.getConnection(DB_URL, Brukernavn, Passord));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
