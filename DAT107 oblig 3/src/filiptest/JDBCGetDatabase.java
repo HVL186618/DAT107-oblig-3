6614bb9ee765feddbde59ea13fd257a85143325c
HVL186618 <186618@stud.hvl.no>
2025-03-25 13:35:13 +0100

    Update JDBCGetDatabase.java

--- DAT107 oblig 3/src/filiptest/JDBCGetDatabase.java
+++ DAT107 oblig 3/src/filiptest/JDBCGetDatabase.java
@@ -8,7 +8,7 @@ public class JDBCGetDatabase {
 	static final String JDBC_Driver = "org.postgresql.Driver";
 	static final String Database = "h186618";
 	static final String Brukernavn = "h186618";
-	static final String Passord = "#HVL_SQL233!"; //legg til "-WITH PASSWORD '#HVL_SQL233!';" i SQL databasen.
+	static final String Passord = "#HVL_SQL233!"; //legg til "-WITH PASSWORD '#HVL_SQL233!';" inni PGAdmin for å få tilgang med dette passordet.
 	
 	static final String Tjener_og_port = "ider-database.westeurope.cloudapp.azure.com:5433"; //Måtte legge til PSQL port for at dette skulle fungere. Enten 5433 eller 5432
 	static final String DB_URL = "jdbc:postgresql://" + Tjener_og_port + "/" + Database;
