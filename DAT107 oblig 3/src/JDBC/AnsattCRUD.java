e50184c5923377be78dc58c55ed03908f89db2ce
186617 <186617@stud.hvl.no>
2025-04-02 13:28:12 +0200

    ansattcrud

--- DAT107 oblig 3/src/JDBC/AnsattCRUD.java
+++ DAT107 oblig 3/src/JDBC/AnsattCRUD.java
@@ -6,17 +6,30 @@ public interface AnsattCRUD {
 	//Oppretter ny Ansatt rad.
 	void lagreAnsatt(Ansatt a);
 	
+	/** Read1 - Hente ut data fra databasen. En enkelt. */
+	Ansatt hentAnsatt(int id);
+	
 	//Henter ut data fra databasen. En enkelt
 	Ansatt hentAnsatt(String id);
 	
 	//Henter ut data fra databasen. Alle.
 	List<Ansatt> hentAlleAnsatt();
 	
+	/** Read3 - Hente ut data fra databasen. Named Query. */
+	List<Ansatt> hentAlleAnsattNQ();
+	
+	/** Update1 - Oppdatere en person-rad i databasen. */
+	void oppdaterAnsatt(Ansatt p);
+	
+	/** Update2 - Oppdatere en person-rad i databasen. */
+	void oppdaterAnsatt(int id, String nyttNavn);
+	
+	/** Delete - Slette en person-rad fra databasen. */
+	void slettAnsatt(int id);
+	
 	String søkID();
 	
 	String søkBr();
-	
-	
 	
 	
 }
