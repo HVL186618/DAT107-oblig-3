8e6cc32dfe3f0c77df401bf1731a59b5a4efe3ff
LuciferDeFallen <186611@stud.hvl.no>
2025-04-02 13:38:30 +0200

    ansatt

--- DAT107 oblig 3/src/JDBC/Ansatt.java
+++ DAT107 oblig 3/src/JDBC/Ansatt.java
@@ -1,20 +1,42 @@
 package JDBC;
 
+import java.sql.*;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
 
-@Entity
 public class Ansatt {
-	public String id;
-	public Ansatt(String id) {
+	
+	@Id	private Integer id;
+	private String navn;
+	
+	public Ansatt() {}
+	
+	public Ansatt(Integer id, String navn) {
 		this.id = id;
-	}
-	public String getId() {
-		//TODO
-		return id;
-		
+		this.navn = navn;
 	}
 	
-	public void setId(String id) {
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
 		this.id = id;
 	}
+	public String getNavn() {
+		return navn;
+	}
+	public void setNavn(String navn) {
+		this.navn = navn;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Person: id=%d, navn=%s", id, navn);
+	}
+    
+    public static void main(String[] args) {
+    	
+    }
 }
