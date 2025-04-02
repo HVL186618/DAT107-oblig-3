c53b11554ab0e58338b68d37f355f663d1f1162a
HVL186618 <186618@stud.hvl.no>
2025-04-02 13:07:15 +0200

    Create main.java

---
+++ DAT107 oblig 3/src/JDBC/main.java
@@ -0,0 +1,28 @@
+package JDBC;
+
+import java.sql.Connection;
+
+public class main {
+	static final String JDBC_Driver = "org.postgresql.Driver";
+	static final String Database = "h186618";
+	static final String Brukernavn = "h186618";
+	static final String Passord = "#HVL_SQL233!"; //legg til "-WITH PASSWORD '#HVL_SQL233!';" inni PGAdmin for å få tilgang med dette passordet.
+	
+	static final String Tjener_og_port = "ider-database.westeurope.cloudapp.azure.com:5433"; //Måtte legge til PSQL port for at dette skulle fungere. Enten 5433 eller 5432
+	static final String DB_URL = "jdbc:postgresql://" + Tjener_og_port + "/" + Database;
+	public static int active = 1;
+	public static Connection conn;
+	public static java.sql.Statement stmnt;
+	public static void main(String[] args) throws ClassNotFoundException {
+		// TODO Auto-generated method stub
+		String SQL = "SELECT * FROM SchemaAnsatt.Ansatt"; //FORMAT: SELECT * from 'navn på schema som tilhører tabell'.'navn på tabell'
+		
+		conn = null;
+		stmnt = null;
+		Class.forName(JDBC_Driver);
+		System.out.println(">Kobler til database...");
+		System.out.println(">...");
+
+	}
+
+}
