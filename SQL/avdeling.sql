bf1fa6d81bfe6f56a33fbf32aa1c9356cdccaa21
HVL186618 <186618@stud.hvl.no>
2025-04-03 14:08:35 +0200

    Update avdeling.sql

--- SQL/avdeling.sql
+++ SQL/avdeling.sql
@@ -1,8 +1,8 @@
 create Schema SchemaAvdeling;
-set search_path to SchemaAvdeling;
+set search_path to SchemaAnsatt;
 CREATE TABLE Avdeling
 (
     avdelingID varchar(40) primary key,
     navn varchar(30),
-    sjefID varchar(40) foreign key references Ansatt (ansattID), --Burde hente ID til personen som er sjef og bør være foreign key
+    sjefID varchar(40) references Ansatt (ansattID), --Burde hente ID til personen som er sjef og bør være foreign key
 );
