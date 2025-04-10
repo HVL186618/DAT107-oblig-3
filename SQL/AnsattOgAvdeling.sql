217289347f66b6db6d7b3c0e09798d27d33cb3c0
HVL186618 <186618@stud.hvl.no>
2025-04-10 12:30:52 +0200

    Update AnsattOgAvdeling.sql

--- SQL/AnsattOgAvdeling.sql
+++ SQL/AnsattOgAvdeling.sql
@@ -1,23 +1,23 @@
 -- CREATE SCHEMA SchemaAnsatt;
 SET search_path TO SchemaAnsatt;
 
-drop table if exists Avdeling;
+drop table if exists Avdeling cascade; --Cascade gjør at det sletter alle tabeller som er avhengi på denne.
 CREATE TABLE Avdeling (
-    avdelingID varchar(40) PRIMARY KEY,
+    avdelingID SERIAL PRIMARY KEY,
     navn varchar(30),
-    sjefID varchar(40) -- placeholder, FK added later
+    sjefID SERIAL -- placeholder, FK added later
 );
 
 drop table if exists Ansatt;
 CREATE TABLE Ansatt (
-    ansattID varchar(40) PRIMARY KEY,
+    ansattID SERIAL PRIMARY KEY,
     brukernavn varchar(4),
     fornavn varchar(30),
     etternavn varchar(20),
     dato timestamp,
     stilling varchar(15),
     månedslønn varchar(30),
-    avdelingID varchar(40) REFERENCES Avdeling(avdelingID),
+    avdelingID SERIAL REFERENCES Avdeling(avdelingID),
     prosjekter varchar(30)
 );
 
