975fcca5edec2d96207b7771a2426b173c3c78f1
HVL186618 <186618@stud.hvl.no>
2025-04-22 12:59:11 +0200

    Update InsertInto.sql

--- SQL/InsertInto.sql
+++ SQL/InsertInto.sql
@@ -1,1 +1,6 @@
-select * from SchemaAnsatt.Avdeling
+INSERT INTO SchemaAnsatt.Ansatt (brukernavn, fornavn, etternavn, dato, stilling, månedslønn, avdeling, prosjekter)
+VALUES ('Br1', 'Ola', 'Nordmann', DEFAULT, 'ansatt', '1000 kr', null, 'prosjekt 1'),
+('Br2','Kari', 'Nordmann', DEFAULT, 'ansatt', '1000 kr', null, 'prosjekt 1');
+
+INSERT INTO SchemaAnsatt.Avdeling (navn, sjefID)
+values ('Avdeling 1', '4');
